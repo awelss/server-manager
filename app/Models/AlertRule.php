96ed13090b330dfaa35d5a2c786d343cde49cd57
awelss <4welqq@gmail.com>
2026-09-11 21:37:06 +0700

    feat: add smart alert state

--- app/Models/AlertRule.php
+++ app/Models/AlertRule.php
@@ -12,15 +12,28 @@ class AlertRule extends Model
         'metric',
         'operator',
         'threshold',
+        'for_minutes',
         'cooldown_minutes',
+        'recovery_enabled',
         'whatsapp_number',
         'enabled',
+        'breach_started_at',
+        'is_active',
+        'last_recovered_at',
+        'last_value',
     ];
 
     protected $casts = [
         'threshold' => 'double',
+        'for_minutes' => 'integer',
+        'cooldown_minutes' => 'integer',
+        'recovery_enabled' => 'boolean',
         'enabled' => 'boolean',
         'last_triggered_at' => 'datetime',
+        'breach_started_at' => 'datetime',
+        'is_active' => 'boolean',
+        'last_recovered_at' => 'datetime',
+        'last_value' => 'double',
     ];
 
     public function user()
@@ -39,7 +52,7 @@ class AlertRule extends Model
             return false;
         }
 
-        return $this->last_triggered_at->addMinutes($this->cooldown_minutes)->isFuture();
+        return $this->last_triggered_at->copy()->addMinutes($this->cooldown_minutes)->isFuture();
     }
 
     public function evaluate(float $value): bool
